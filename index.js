5d64074ca55e52af863e1c1f9a2ae3674528fa4e
anubhuti-sh <anubhutiv@sensehawk.com>
2020-08-27 15:57:56 +0530

    fix port

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.listen(4001, () => {
+let port = NODE_ENV=="stage" ? 4001 : 4000;
+
+app.listen(port, () => {
     console.log(`running ${process.env.NODE_ENV} version ${Math.random()}`);
 });
